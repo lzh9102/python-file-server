4512ca713cc1586e89db52fff4216da726e46b06
Timothy Lin <lzh9102@gmail.com>
2011-11-18 22:22:01 +0800

    "Mount" the virtual filesystem under http://hostname/PREFIX. Redirect http://hostname/ to http://hostname/PREFIX Search the data file in OPT_DATA_PATH Don't wait for running threads when shutdown. Added send_file member function. Added prefix, strip_prefix and strip_suffix functions.

--- python-hfs.py
+++ python-hfs.py
@@ -24,6 +24,21 @@ OPT_ROOT_DIR = ""
 # the number of bytes to read in a chunk when reading a file
 OPT_CHUNK_SIZE = 1024
 
+# the prefix to add before the root directory
+# For example, if PREFIX is "/root" and the host is 127.0.0.1, then
+# the root directory is http://127.0.0.1/root
+PREFIX = "/f"
+
+# the path to search for data files.
+# This path is located on the host filesystem.
+OPT_DATA_PATH = "data"
+
+# correct potential errors
+if OPT_DATA_PATH.endswith('/'): # strip trailing '/' after data path
+    OPT_DATA_PATH = OPT_DATA_PATH[0:len(OPT_DATA_PATH)-1]
+if not PREFIX.startswith('/'):
+    PREFIX = '/' + PREFIX
+
 ###### Helper Functions ######
 
 def directory_exists(path):
@@ -39,6 +54,45 @@ def is_file(path):
         return False
     mode = os.stat(path).st_mode
     return not stat.S_ISDIR(mode)
+
+def prefix(path):
+    """ Get the top-level folder in path.
+        For example, the output for "/usr/bin/python" will be "/usr" """
+    slash_index = path[1:].find('/')
+    if slash_index >= 0:
+        return path[0:slash_index+1]
+    else:
+        return path
+    
+def strip_prefix(path):
+    """ Remove the top-level folder name in path
+        For example, if path is "/usr/bin/python", the output will be "/bin/python"
+        If the input is "/", then the output will be also "/".
+        """
+    result = path
+    if result[0] == '/':
+        result = result[1:]
+    slash_index = result.find('/')
+    
+    if slash_index >= 0:
+        result = result[slash_index:]
+    else:
+        result = ""
+        
+    if len(result) == 0: # '/' should be translated to '/'
+        result = "/"
+    return result
+
+def strip_suffix(path):
+    result = path
+    if result.endswith("/"):
+        result = result[0:len(result)-1]
+    slash_index = result.rfind('/')
+    if slash_index >= 0:
+        result = result[0:slash_index]
+    if len(result) == 0:
+        result = '/'
+    return result
 
 def concat_folder_file(folder, file):
     """ Concatenate folder name with file name.
@@ -86,6 +140,7 @@ FILE_NOT_FOUND_TEMPLATE = """
 HTTP_OK = 200
 HTTP_NOCONTENT = 204
 HTTP_NOTFOUND = 404
+HTTP_MOVED_PERMANENTLY = 301
 
 class ThreadedHttpServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
     """ This class combines two classes ThreadingMixIn and BaseHTTPServer.HTTPServer
@@ -97,49 +152,63 @@ class MyServiceHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         """ Handle http GET request from client. """
         path = urllib.unquote(self.path)
-        full_path = OPT_ROOT_DIR + path
         host = "http://" + self.headers["host"]
         
-        print("Request File: " + full_path)
+        print("Request File: " + path)
         
-        if directory_exists(full_path):
-            """ Handle directory listing. """
-            self.send_response(HTTP_OK)
-            self.send_header("Content-Type", "text/html;charset=UTF-8")
-            self.end_headers()
+        if prefix(path) == PREFIX:
+            """ Handle Virtual Filesystem """
+            # strip path with PREFIX
+            path = strip_prefix(path)
+            full_path = OPT_ROOT_DIR + path           
             
-            content = self.generate_folder_listing(host, OPT_ROOT_DIR, path)
+            if directory_exists(full_path):
+                """ Handle directory listing. """
+                self.send_response(HTTP_OK)
+                self.send_header("Content-Type", "text/html;charset=UTF-8")
+                self.end_headers()
+                
+                content = self.generate_folder_listing(host, OPT_ROOT_DIR, path)
+                
+                self.wfile.write(content)
+                
+            elif is_file(full_path):
+                """ Handle file downloading. """
+                self.send_file(full_path)
+                
+            else:
+                """ Handle File Not Found error. """
+                self.send_response(HTTP_OK)
+                self.send_header("Content-Type", "text/html;charset=UTF-8")
+                self.end_headers()
+                
+                content = FILE_NOT_FOUND_TEMPLATE % {"FILE": path}
+                
+                self.wfile.write(content)
+        elif path == "/": # redirect '/' to /PREFIX
+            self.send_response(HTTP_MOVED_PERMANENTLY) # redirect
+            self.send_header("Location", host + PREFIX)
+        else: # data file
+            full_path = OPT_DATA_PATH + path
+            print("Request Data File: " + full_path)
+            if is_file(full_path):
+                self.send_file(full_path)
             
-            self.wfile.write(content)
-            
-        elif is_file(full_path):
-            """ Handle file downloading. """        
-            self.send_response(HTTP_OK)
-            
-            type,encoding = mimetypes.guess_type(full_path)
-            self.send_header("Content-Type", "%(TYPE)s;charset=%(ENCODING)s" % \
-                             {"TYPE": type, "ENCODING": encoding})
-            self.end_headers()
-            
-            # Read the file and send it to the client
-            with open(full_path, "rb") as f:
-                while 1:
-                    chunk = f.read(OPT_CHUNK_SIZE)
-                    if chunk:
-                        self.wfile.write(chunk)
-                    else:
-                        break
-            
-        else:
-            """ Handle File Not Found error. """
-            self.send_response(HTTP_OK)
-            self.send_header("Content-Type", "text/html;charset=UTF-8")
-            self.end_headers()
-            
-            content = FILE_NOT_FOUND_TEMPLATE % {"FILE": path}
-            
-            self.wfile.write(content)
-            
+    def send_file(self, filename):
+        """ Read the file and send it to the client. """
+        self.send_response(HTTP_OK)
+        type,encoding = mimetypes.guess_type(filename)
+        self.send_header("Content-Type", "%(TYPE)s;charset=%(ENCODING)s" % \
+                         {"TYPE": type, "ENCODING": encoding})
+        self.end_headers()
+        with open(filename, "rb") as f:
+            while 1:
+                chunk = f.read(OPT_CHUNK_SIZE)
+                if chunk:
+                    self.wfile.write(chunk)
+                else:
+                    break
+    
     def generate_parent_link(self, host, folder):
         """ Generate link for the parent directory of "folder" """
         parent_dir = folder
@@ -156,16 +225,16 @@ class MyServiceHandler(SimpleHTTPRequestHandler):
         if folder == "/": # already at root directory
             return "<u>Up</u>"
         else:
-            return "<a href='" + concat_folder_file(host, parent_dir) + "'>Up</a>"
+            return "<a href='" + host + concat_folder_file(PREFIX, parent_dir) + "'>Up</a>"
             
     
     def generate_home_link(self, host):
         """ Generate link for root directory """
-        return "<a href='" + host + "'>Home</a>"
+        return "<a href='" + host + PREFIX + "'>Home</a>"
             
     def generate_link(self, host, root, folder, file):
         """ Generate html link for a file/folder. """
-        link = host + concat_folder_file(folder, file)
+        link = host + PREFIX + concat_folder_file(folder, file)
         return "<a href='%(LINK)s'>%(NAME)s</a> " % \
             {"LINK": link, "NAME": cgi.escape(file)}
                 
@@ -235,6 +304,7 @@ except ValueError:
     
 try:
     server = ThreadedHttpServer(('', OPT_PORT), MyServiceHandler)
+    server.daemon_threads = True
     server.serve_forever()
 except socket.error, e:
     if e.errno == 13: # permission denied
